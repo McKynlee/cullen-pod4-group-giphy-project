b5dd7f109a7fa34b570e1395e5281bb00b42d332
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-04 10:24:49 -0600

    Installing all the things

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import axios from 'axios';
+
+// Redux-saga:
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+ReactDOM.render(<Provider store={ }><App /></Provider>, document.getElementById('root'));
