d232f041df0182d4581bf6eb643d884a5b157b1f
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-04 13:37:45 -0600

    Changing dropdown values to match category IDs

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 function Favorites() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,20 +13,32 @@ function Favorites() {
     console.log('fetching favorites');
   }
 
+  // Variable to capture selected value in dropdown:
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleCategoryChange = (userSelection) => {
+    console.log('handleCategoryChange', selectedCategory);
+    setSelectedCategory(userSelection);
+  }
+
   return (
     <>
       <h1>Your favorite GIFs!</h1>
       {/* selector for categories */}
       {/* will map through search results and render dropdown
       with each item */}
-      <select>
+      <select name="category"
+        value={selectedCategory}
+        id="category"
+        onChange={(event) => handleCategoryChange(event.target.value)}>
         <option value="">Choose Category</option>
-        <option value="funny">Funny</option>
-        <option value="cohort">Cohort</option>
-        <option value="cartoon">Cartoon</option>
-        <option value="nsfw">NSFW</option>
-        <option value="meme">Meme</option>
+        <option value="1">Funny</option>
+        <option value="2">Cohort</option>
+        <option value="3">Cartoon</option>
+        <option value="4">NSFW</option>
+        <option value="5">Meme</option>
       </select>
+      <div>{selectedCategory}</div>
     </>
   )
 }
